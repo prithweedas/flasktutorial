4fcdb7a301c65dd9384a566d727bd61cc6b76690
Prithwee Das <prithweedas123@gmail.com>
2019-07-09 12:44:37 +0530

    database models created for User and Post

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 import os
+from flask_sqlalchemy import SQLAlchemy
 from flaskblog.users import users_blueprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['FLASK_BLOG_SECRET']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['FLASK_BLOG_DB_URL']
 app.register_blueprint(users_blueprint)
+db = SQLAlchemy(app)
 
 from flaskblog import routes
+from flaskblog.users import models
--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 export FLASK_APP=init.py
 export FLASK_DEBUG=1
 export FLASK_BLOG_SECRET='fghfytdfuyfdjhdyhgkuguyjhik'
+export FLASK_BLOG_DB_URL='sqlite:///../site.db'
 flask run
